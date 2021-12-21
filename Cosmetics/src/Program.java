3b4c55447c11bbc5c1a6fb3b462c07d0ac09895e
margarita-gagarinskaya <barnie3121@gmail.com>
2021-12-21 15:50:18 +0300

    Редактирование методов

--- Cosmetics/src/Program.java
+++ Cosmetics/src/Program.java
@@ -1,9 +1,14 @@
 public class Program {
     public static void main(String[] args) {
 
-        Cosmetic Revolution = new Cosmetic("Revolution"); // создание объекта
-        Revolution.displayInfo();
-        Revolution.setPrice(500);
-        Revolution.displayInfo();
-    }}
+        Cosmetic Revolution = new Cosmetic("Revolution","China"); // создание объекта бренд Revolution
+            Revolution.setPrice(500);
+            //Revolution.setCountry("China");
+            Revolution.displayInfo();
+        Cosmetic NIVEA = new Cosmetic("NIVEA","Germany"); // создание объекта бренд NIVEA
+            NIVEA.setPrice(150);
+            NIVEA.setCountry("Germany");
+            NIVEA.displayInfo();
+       }
+}
 
