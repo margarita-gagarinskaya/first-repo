a3dba4b65be396608a0b3ff43b1c1a8cf1f0d28f
margarita-gagarinskaya <barnie3121@gmail.com>
2021-12-21 22:33:03 +0300

    Добавление класса "Декоративный"

--- Cosmetics/src/Program.java
+++ Cosmetics/src/Program.java
@@ -1,13 +1,22 @@
 public class Program {
     public static void main(String[] args) {
+        final String TYPE1 = "(Декоративная)";
+        final String TYPE2 = "(Уходовая)";
 
-        Cosmetic Revolution = new Cosmetic("Revolution","China"); // создание объекта бренд Revolution
-            Revolution.displayInfo();
+        Cosmetic Revolution = new Cosmetic("Revolution "+TYPE1,"China"); // создание объекта бренд Revolution
+        Revolution.displayInfo();
+
         Cosmetic NIVEA = new Cosmetic(); // создание объекта бренд NIVEA
             //NIVEA.setPrice(150);
-            NIVEA.setNameBrand("NIVEA");
+            NIVEA.setNameBrand("NIVEA "+TYPE2);
             NIVEA.setCountry("Germany");
             NIVEA.displayInfo();
-       }
+
+        Decorative Memo = new Decorative();
+        Memo.Information();
+        String S = Memo.getInfo(); //в переменной S результат отработки метода
+        System.out.println(S);
+
+     }
 }
 
