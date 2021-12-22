4bfd944ec087af48b0831aa340f65ea212f88f45
margarita-gagarinskaya <barnie3121@gmail.com>
2021-12-22 14:13:26 +0300

    Создание класса Care

---
+++ Cosmetics/src/Care.java
@@ -0,0 +1,21 @@
+public class Care extends Cosmetic {
+    protected String Info;
+
+    public Care() {
+        //конструктор по умолчанию
+    }
+    public void setInfo(String info) {
+        Info = info;
+    }
+    public void Information2() {
+        this.setInfo("""
+                Уходовая косметика предназначена для ухода за кожей лица,
+                тела, ногтями и волосами. Она создана для поддержания красоты
+                и молодости кожи, а также для решения различных косметических
+                проблем – от повышенной жирности до преждевременного старения.
+                """);
+    }
+    public String getInfo() {
+        return Info;
+    }
+}
