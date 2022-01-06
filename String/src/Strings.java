e15e82d751707caa49773697edf661a32e83c897
margarita-gagarinskaya <barnie3121@gmail.com>
2022-01-06 16:25:47 +0300

    Замена гласных на порядковый номер от 1 до 10

--- String/src/Strings.java
+++ String/src/Strings.java
@@ -3,21 +3,17 @@ public class Strings {
     public static String changeString(String oldText) {
         int leng = oldText.length();
         System.out.printf("Длина входящей строки: %d\n", oldText.length());
-        int i = leng;
-        do{
             String replText = oldText.replace ("а", "1");
-            String replText1 = replText.replace ("е", "6");
-            String replText2 = replText1.replace ("ё", "7");
-            String replText3 = replText2.replace ("и", "10");
-            String replText4 = replText3.replace ("o", "16");
-            String replText5 = replText4.replace ("у", "21");
-            String replText6 = replText5.replace ("ы", "29");
-            String replText7 = replText6.replace ("э", "31");
-            String replText8 = replText7.replace ("ю", "32");
-            String replText9 = replText8.replace ("я", "33");
-            i--;
-        }
-        while (i > 0);
-        return oldText;
+            String replText1 = replText.replace ("е", "2");
+            String replText2 = replText1.replace ("ё", "3");
+            String replText3 = replText2.replace ("и", "4");
+            String replText4 = replText3.replace ("о", "5");
+            String replText5 = replText4.replace ("у", "6");
+            String replText6 = replText5.replace ("ы", "7");
+            String replText7 = replText6.replace ("э", "8");
+            String replText8 = replText7.replace ("ю", "9");
+
+
+        return replText8.replace ("я", "10");
     }
 }
