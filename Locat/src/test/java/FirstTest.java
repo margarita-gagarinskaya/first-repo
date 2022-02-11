4cdf805275732c124f5836472840248f764ae5ed
margarita-gagarinskaya <barnie3121@gmail.com>
2022-02-11 21:24:59 +0300

    Отправка письма

--- Locat/src/test/java/FirstTest.java
+++ Locat/src/test/java/FirstTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -15,7 +14,8 @@ public class FirstTest {
         String url = "https://mail.yandex.ru/?uid=1561621129#inbox";
         String address = "gagarinskayam@yandex.ru";
         String password = "Jpldmc765!_mdikfco";
-
+        String whom = "barnie3121@gmail.com";
+        String textOfMassage= "Отправляется тестовое письмо в автоматическом режиме";
         System.setProperty("webdriver.chrome.driver", "D:/Program Files/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
 
@@ -36,11 +36,24 @@ public class FirstTest {
         WebElement next1 = driver.findElement(By.cssSelector("button[data-t]"));
         next1.click();
 
-//        WebElement notNow = driver.findElement(By.cssSelector("div[data-t*=phone_skip] >button"));
-//        notNow.click();
+        WebElement pressWrite = (new WebDriverWait(driver,  Duration.ofSeconds(25)))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.mail-ComposeButton-Text")));
+        pressWrite.click();
 
+        WebElement message = (new WebDriverWait(driver,  Duration.ofSeconds(20)))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[class*=cke_wysiwyg_div] >div")));
+        message.click();
+        message.sendKeys(textOfMassage);
 
+        WebElement toWhom = (new WebDriverWait(driver,  Duration.ofSeconds(10)))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[contenteditable]")));
+        toWhom.click();
+        toWhom.sendKeys(whom);
 
+        WebElement send = (new WebDriverWait(driver,  Duration.ofSeconds(10)))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.ComposeControlPanel-Part " +
+                        ">div >button[type=button]")));
+        send.click();
 
 
 
@@ -48,3 +61,5 @@ public class FirstTest {
     }
 
 }
+//div.ns-view-compose-buttons-box." +
+//        "ns-view-id-95 > div > div > div > a
